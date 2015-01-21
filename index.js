9eb1fe2dd4a209adc15ddecf33bd591d722469e3
PaulGuo <badkaikai@gmail.com>
2015-01-21 23:22:51 +0800

    update for data deep walk.

--- index.js
+++ index.js
@@ -3,6 +3,22 @@ var fs = require('fs');
 var path = require('path');
 
 module.exports = function(tplPath, options, fn) {
+    var deep = function(data, scope, _tmp) {
+        _tmp = data;
+        scope = scope.replace(/\[([^\]]+)\]/igm, '.$1');
+        scope = scope.split('.');
+
+        for(var i = 0; i < scope.length; i++) {
+            scope[i] = scope[i].replace(/["']/igm, '');
+            if(scope[i] === '_') {
+                continue;
+            }
+
+            _tmp = _tmp[scope[i]];
+        }
+        return _tmp;
+    };
+
     var includeFileDetect = function(str) {
         var includeWithoutRender = juicer.tags.operationOpen + 'include\\s*([^}]*?)\\s*' + juicer.tags.operationClose;
         juicer.settings.includeWithoutRender = new RegExp(includeWithoutRender, 'igm');
@@ -13,7 +29,7 @@ module.exports = function(tplPath, options, fn) {
                     tpl = tpl.substr(7);
                     tpl = path.resolve(path.dirname(tplPath), tpl);
                     tpl = fs.readFileSync(tpl, 'utf8');
-                    data === '_' ? data = options : data = options[data];
+                    data === '_' ? data = options : data = deep(options, data);
                     return juicer(includeFileDetect(tpl), data);
                 }
 
